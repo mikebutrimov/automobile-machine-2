b2dfa3f39da1cf0037cb762a46eb4334724bc91d
Mike Butrimov <mike.butrimov@gmail.com>
2017-04-18 20:15:51 +0300

    ARDUINO DUE branch

--- arduino/am2/am2.ino
+++ arduino/am2/am2.ino
@@ -1,5 +1,5 @@
-#define GPIO2_PREFER_SPEED 1
-#include "arduino2.h"
+//#define GPIO2_PREFER_SPEED 1
+//#include "arduino2.h"
 #include <mcp_can.h>  
 #include <pb_encode.h>
 #include <pb_decode.h>
@@ -16,6 +16,11 @@ const int VDOWNPIN = 33;
 Button vUpButton = Button(VUPPIN);
 Button vDownButton = Button(VDOWNPIN);
 
+const int AINETIN = 2;
+const int AINETOUT = 13;
+uint8_t i,j,type;
+const int bytes = 11;
+bool fast_byte_buffer[88];
 
 int counts = 0;
 byte bits = 0;
@@ -45,11 +50,81 @@ int vol_index = 15; // default volume index to restore
 
 
 
+
+
+void crc(uint8_t *packet) {
+  uint8_t crc_reg=0xff,poly,i,j;
+  uint8_t bit_point, *byte_point;
+  for (i=0, byte_point=packet; i<10; ++i, ++byte_point) {
+    for (j=0, bit_point=0x80 ; j<8; ++j, bit_point>>=1) {
+      if (bit_point & *byte_point) { // case for new bit =1
+        if (crc_reg & 0x80) poly=1; // define the polynomial
+        else poly=0x1c;
+        crc_reg= ( (crc_reg << 1) | 1) ^ poly;
+      } else { // case for new bit =0
+        poly=0;
+        if (crc_reg & 0x80) poly=0x1d;
+        crc_reg= (crc_reg << 1) ^ poly;
+      }
+    }
+  }
+  packet[10]= ~crc_reg; // write use CRC
+}
+
+
+void fastSend(bool* packet, int packet_size, bool ack){
+  noInterrupts();
+  if (!ack){
+    digitalWrite(AINETOUT, HIGH);
+    delayMicroseconds(32);
+    digitalWrite(AINETOUT, LOW);
+    delayMicroseconds(16);
+  }
+  for (int i = 0; i< packet_size; i++){
+    if (packet[i] == 0){
+      digitalWrite(AINETOUT, HIGH);
+      delayMicroseconds(14);
+      digitalWrite(AINETOUT, LOW);
+      delayMicroseconds(8);
+    }
+    else {
+      digitalWrite(AINETOUT, HIGH);
+      delayMicroseconds(5);
+      digitalWrite(AINETOUT, LOW);
+      delayMicroseconds(18);
+    }
+  }
+  interrupts();
+}
+
+void sendAiNetCommand(byte * packet, int packet_size){
+  noInterrupts();
+  for (i=0;i<packet_size;i++) {
+    for (j=0;j<8;j++) {
+      type=(packet[i] & (1 << (7-j))) >> (7-j);
+      if (type==0) {
+        fast_byte_buffer[i*8+j] = 0;
+      }
+      else {
+        fast_byte_buffer[i*8+j] = 1;
+      }
+    }
+  }
+  fastSend(fast_byte_buffer, packet_size*8, 0);
+  interrupts();
+}
+
+
+
+
+
+
+
 void isr_read_msg(){
   if (!readyForNext) return;
   noInterrupts();
   //here we start with RISING on SOF but must check it
-  while (digitalRead2(AINETIN) == HIGH  && counts < BRAKE){
+  while (digitalRead(AINETIN) == HIGH  && counts < BRAKE){
     counts++;
   }
   if (counts<SOF_THRESHOLD){//need to do some finetuning
@@ -57,10 +132,10 @@ void isr_read_msg(){
   }
   //SOF ends with LOW reading. wait for HIGH again and count it
   for (int i = 0; i< bytes*8; i++){
-    while (digitalRead2(AINETIN) == LOW){
+    while (digitalRead(AINETIN) == LOW){
     }
     counts = 0;
-    while (digitalRead2(AINETIN) == HIGH && counts < BRAKE ){
+    while (digitalRead(AINETIN) == HIGH && counts < BRAKE ){
       counts++;
     }
     if (counts < BITVAL_THRESHOLD_HIGH){
@@ -369,8 +444,8 @@ void batch_send(CAN_COMMAND * cmds, int len){
  
 void setup() {
   //init ainet pins
-  pinMode2(AINETIN, INPUT); 
-  pinMode2(AINETOUT, OUTPUT);
+  pinMode(AINETIN, INPUT); 
+  pinMode(AINETOUT, OUTPUT);
   //prepare uranus;
   //attachInterrupt(digitalPinToInterrupt(AINETIN), isr_read_msg, RISING);
   Serial.begin(115200);
